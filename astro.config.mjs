7388a3798ac7ae3cce2c292f6460eae9a3d7bc18
figgyhunter <leonardo.roi3@gmail.com>
2023-07-23 23:01:28 +0200

    updated astro config for latest features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), vue()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), vue(), compress()],
+  experimental: {
+    assets: true,
+  },
 });
