98f4b86fc82752fe575dc544df4ce7c1c1afecd6
figgyhunter <leonardo.roi3@gmail.com>
2023-07-29 22:56:07 +0200

    worked on image fullscreen component

--- src/components/ImageFullScreen.vue
+++ src/components/ImageFullScreen.vue
@@ -6,7 +6,7 @@
         @click="doLogic"
         height="800"
         width="800"
-        alt="nesto"
+        :alt="photo.attributes.image.data.attributes.caption"
         :srcset="getSrcSetFromPhoto(photo)"
         sizes="(max-width: 500px) 25vw, (max-width: 1200px) 50vw, 100vw"
         :data-info="JSON.stringify(photo.attributes.metadata)"
@@ -17,8 +17,26 @@
   </section>
 
   <Transition name="show" mode="in-out">
-    <div v-if="currentMetaData" class="image-viewer">
-      <img :srcset="currentSrcset" :sizes="currentSizes" alt="nesto" />
+    <div
+      v-if="fullScreenShown"
+      @click="fullScreenShown = false"
+      class="image-viewer-container"
+    >
+      <div class="image-viewer">
+        <button class="close" @click="fullScreenShown = false">X</button>
+        <img
+          @click.prevent.capture="stopPropagation"
+          :srcset="currentSrcset"
+          :sizes="currentSizes"
+          alt="nesto"
+        />
+        <section class="image-viewer-description">
+          <h4 @click.prevent.capture="stopPropagation">{{ currentAlt }}</h4>
+          <p @click.prevent.capture="stopPropagation">
+            {{ currentMetaData.device }}
+          </p>
+        </section>
+      </div>
     </div>
   </Transition>
 </template>
@@ -26,7 +44,7 @@
 <script setup lang="ts">
 import { getPhotographs } from "@/api/photographs/getPhotographs";
 import { getSrcSetFromPhoto } from "@/api/photographs/getSrcSetFromPhoto";
-import { onBeforeMount, onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const photos = await getPhotographs();
 
@@ -35,12 +53,21 @@ getSrcSetFromPhoto(photos[0]);
 const currentMetaData = ref(null);
 const currentSrcset = ref("");
 const currentSizes = ref("");
+const fullScreenShown = ref(false);
+const currentAlt = ref("");
+
+const stopPropagation = function (e) {
+  e.stopPropagation();
+};
 
 const doLogic = function (e) {
+  fullScreenShown.value = true;
+  currentAlt.value = e.target.alt;
   currentSrcset.value = e.target.srcset; // Set the srcset based on the selected photo
   currentSizes.value =
     "(max-width: 500px) 100vw, (max-width: 1200px) 80vw, 50vw"; // Set the sizes based on the selected photo (or retrieve it from the photo object)
   currentMetaData.value = JSON.parse(e.target.getAttribute("data-info"));
+  console.log(currentMetaData.value);
 };
 
 onMounted(() => {
@@ -74,18 +101,60 @@ onMounted(() => {
   }
 }
 .image-viewer {
-  position: fixed;
-  z-index: 15;
-  width: 100vw;
-  min-height: 100vh;
-  background-color: blueviolet;
-  top: 0;
-  left: 0;
   display: grid;
+  grid-template-rows: auto max-content;
+  @media screen and (min-width: 1200px) {
+    grid-template-rows: unset;
+    grid-template-columns: auto auto;
+  }
+  height: 100%;
+  align-content: center;
+  &-container {
+    position: fixed;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    z-index: 110;
+    background-color: rgba(0, 0, 0, 0.914);
+    padding-bottom: 2rem;
+  }
+
+  &-description {
+    display: grid;
+    position: initial;
+    color: white;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+
+    align-self: start;
+    align-content: center;
+    h4 {
+      font-family: "Montserrat Bold";
+    }
+  }
+
+  width: 80%;
+  margin: 0 auto;
+
   img {
-    place-self: center;
+    place-self: center start;
     max-height: 75vh;
-    max-width: 85vw;
+    max-width: 90vw;
+    width: 100%;
+  }
+  .close {
+    position: absolute;
+    background-color: blue;
+    justify-self: center;
+    bottom: 1.5rem;
+    width: fit-content;
+    border-radius: 3rem;
+    padding: 0.5rem 0.75rem;
+    color: white;
+    font-family: "Montserrat Bold";
+    border: none;
   }
 }
 
@@ -114,6 +183,6 @@ onMounted(() => {
 }
 
 .show-leave-active {
-  animation: fade-out 1.3s;
+  animation: fade-out 0.4s;
 }
 </style>
