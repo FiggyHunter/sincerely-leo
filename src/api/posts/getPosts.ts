f641ab651480e2cef7a68efe38f99842906cb7e4
figgyhunter <leonardo.roi3@gmail.com>
2023-07-30 23:01:38 +0200

    adjusted link

--- src/api/posts/getPosts.ts
+++ src/api/posts/getPosts.ts
@@ -1,7 +1,5 @@
 export async function getPosts() {
-  const res = await fetch(
-    "https://cdn.leotg.com/api/posts?populate=categories"
-  );
+  const res = await fetch("https://cdn.leotg.com/api/posts?populate=*");
   const { data: posts } = await res.json();
 
   return posts;
