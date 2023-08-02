ac7299b1c05256d014d470a0bee2fcfddd31266a
figgyhunter <leonardo.roi3@gmail.com>
2023-08-02 22:58:20 +0200

    [Types] Quote

---
+++ src/api/types/Quote.ts
@@ -0,0 +1,11 @@
+interface Quote {
+  id: number;
+  attributes: {
+    Quote: string;
+    Author: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+}
+export default Quote;
