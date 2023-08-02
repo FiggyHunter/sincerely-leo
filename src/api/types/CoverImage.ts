3eec173221e490772ebb1fa0da6bea1ac2311489
figgyhunter <leonardo.roi3@gmail.com>
2023-08-02 22:58:10 +0200

    [Types] Cover Image

---
+++ src/api/types/CoverImage.ts
@@ -0,0 +1,74 @@
+interface CoverImageFormats {
+  thumbnail: {
+    name: string;
+    hash: string;
+    ext: string;
+    mime: string;
+    path: string | null;
+    width: number;
+    height: number;
+    size: number;
+    url: string;
+  };
+  large: {
+    name: string;
+    hash: string;
+    ext: string;
+    mime: string;
+    path: string | null;
+    width: number;
+    height: number;
+    size: number;
+    url: string;
+  };
+  medium: {
+    name: string;
+    hash: string;
+    ext: string;
+    mime: string;
+    path: string | null;
+    width: number;
+    height: number;
+    size: number;
+    url: string;
+  };
+  small: {
+    name: string;
+    hash: string;
+    ext: string;
+    mime: string;
+    path: string | null;
+    width: number;
+    height: number;
+    size: number;
+    url: string;
+  };
+}
+
+interface CoverImage {
+  id: number;
+  attributes: {
+    name: string;
+    alternativeText: string | null;
+    caption: string | null;
+    width: number;
+    height: number;
+    formats: CoverImageFormats;
+    hash: string;
+    ext: string;
+    mime: string;
+    size: number;
+    url: string;
+    previewUrl: string | null;
+    provider: string;
+    provider_metadata: any | null;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
+
+interface CoverImageData {
+  data: CoverImage;
+}
+
+export default CoverImage;
