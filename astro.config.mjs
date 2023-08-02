b55826e5b7fa8631319d1cf9fcad55023d6c66d1
figgyhunter <leonardo.roi3@gmail.com>
2023-08-02 22:58:50 +0200

    site now rendering on the server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
-
 import compress from "astro-compress";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  base: "",
+  site: "https://test.leotg.com",
   integrations: [mdx(), sitemap(), vue(), compress()],
   experimental: {
-    assets: true,
+    assets: true
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
