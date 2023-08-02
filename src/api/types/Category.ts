f816016d8359057c7b37f0bfa50cad8aaa4b70ac
figgyhunter <leonardo.roi3@gmail.com>
2023-08-02 22:58:00 +0200

    [Types] Category

---
+++ src/api/types/Category.ts
@@ -0,0 +1,12 @@
+interface Category {
+  id: number;
+  attributes: {
+    category_name: string;
+    category_image: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+}
+
+export default Category;
