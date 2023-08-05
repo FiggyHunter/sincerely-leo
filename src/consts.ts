1eef5ee729de24d3afaae0a88cbcb26713c8d910
figgyhunter <leonardo.roi3@gmail.com>
2023-08-05 16:27:15 +0200

    updated consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "Sincerely, Leo | Self-Improvement, Coding, Books, Podcasts, and More";
+export const SITE_DESCRIPTION =
+  "This is a place where I talk about leveling up in life, the coding hustle, favorite books, top-notch podcasts, and yes, you'll even find my amateur photography attempts. It's all here, laid-back and real. Let's learn and explore together!";
