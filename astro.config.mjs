7c88378f682ba27271fb201fd70dda9d052bf92d
figgyhunter <leonardo.roi3@gmail.com>
2023-08-05 16:27:41 +0200

    updated astro config to include partytown and new site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,32 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import compress from "astro-compress";
-
 import node from "@astrojs/node";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   base: "",
-  site: "https://test.leotg.com",
-  integrations: [mdx(), sitemap(), vue(), compress()],
+  site: "https://blog.leonardoroic.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    vue(),
+    compress(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   experimental: {
-    assets: true
+    assets: true,
   },
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
+
+
